b7a3334dd6b2fd810b802098da7b3f5dd9e4dd52
EstherRC <a01656060@tec.mx>
2023-10-28 10:00:24 -0600

    front completo

--- LivyChatBot/src/components/TopBar.tsx
+++ LivyChatBot/src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import '../styles/mainStyles.css';
 import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
@@ -8,24 +8,35 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import logoImage from '../images/livwhite.png';
+import { Button, Popover } from '@mui/material';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
   paddingTop: theme.spacing(0.5),
   paddingBottom: theme.spacing(1),
-  // Override media queries injected by theme.mixins.toolbar
   '@media all': {
     minHeight: 100,
   },
 }));
 
 const appBarStyles = {
-  backgroundColor: "#E10098", // Establece el color de fondo de la barra aquí
+  backgroundColor: "#E10098",
 };
 
-
-
 export default function TopBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={appBarStyles}>
@@ -37,9 +48,37 @@ export default function TopBar() {
             <div id="chatbot-text" >ChatBot</div>
           </div>
           <Typography variant="h5" noWrap component="div" sx={{ flexGrow: 1, alignSelf: 'flex-end' }} />
-          <IconButton size="large" aria-label="display more actions" edge="end" color="inherit">
-            <MoreIcon />
-          </IconButton>
+          <div
+            onMouseEnter={handlePopoverOpen}
+            onMouseLeave={handlePopoverClose}
+          >
+            <IconButton
+              size="large"
+              aria-label="display more actions"
+              edge="end"
+              color="inherit"
+              aria-owns={open ? 'mouse-over-popover' : undefined}
+              aria-haspopup="true"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handlePopoverClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <div>¡Hola! Soy un popover de ejemplo.</div>
+          </Popover>
         </StyledToolbar>
       </AppBar>
     </Box>
