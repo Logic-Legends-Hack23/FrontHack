b393be0eec34c685d298fff83a9d10198b397006
FeryRi <a01656047@tec.mx>
2023-10-27 18:56:38 -0600

    si se acepta, está la primera versión del calendario

--- LivyChatBot/src/App.tsx
+++ LivyChatBot/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProminentAppBar from './TopBar'; 
+import Calendar from './Calendar'; 
 
 function App() {
   return (
     <div>
       <ProminentAppBar /> 
+      <Calendar />
     </div>
   );
 }
